b3f2eeab9ec23ec7cb63f7315a3fbc67108367d6
JiriDavid <jiridavidpromise@gmail.com>
2024-04-08 14:13:57 +0530

    created folder structure

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from './ui/navbar';
+import Header from './ui/header';
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-5">
+    <Navbar />
+    <Header />
+    </main>
+  );
+}
